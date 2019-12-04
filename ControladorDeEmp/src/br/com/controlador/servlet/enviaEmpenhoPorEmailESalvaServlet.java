bfd45cd40c75467e351a21b797dfe3ab8b2aabb8
Mauricio Machado <licio.machado@hotmail.com>
2019-12-03 23:49:45 -0300

    ENVIANDO EMAILS COM ANEXO DA FORMA CORRETA !

--- ControladorDeEmp/src/br/com/controlador/servlet/enviaEmpenhoPorEmailESalvaServlet.java
+++ ControladorDeEmp/src/br/com/controlador/servlet/enviaEmpenhoPorEmailESalvaServlet.java
@@ -1,12 +1,17 @@
 package br.com.controlador.servlet;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -33,6 +38,7 @@ import br.com.controlador.jdbc.dao.EmpresaDao;
 import br.com.controlador.jdbc.modelo.Empenho;
 import br.com.controlador.jdbc.modelo.Empresa;
 import br.com.controlador.jdbc.modelo.Usuario;
+
 
 @WebServlet("/enviaEmail")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2,   // 2MB
@@ -86,6 +92,9 @@ public class enviaEmpenhoPorEmailESalvaServlet extends HttpServlet {
 		String fromEmail = "licio.machado.mm@gmail.com";
 		String toEmail = empresa2.getEmail();
 		
+		
+		List<File> uploadedFiles = saveUploadedFiles(request);
+		
 		Properties properties = new Properties();
         properties.put("mail.smtp.host", "smtp.gmail.com");
         properties.put("mail.smtp.socketFactory.port", "465");
@@ -133,7 +142,11 @@ public class enviaEmpenhoPorEmailESalvaServlet extends HttpServlet {
 			
 			//Attachment body part.
 			MimeBodyPart pdfAttachment = new MimeBodyPart();
-			pdfAttachment.attachFile("C:/Users/Mauricio/Desktop/800501.pdf");
+			String fileName = extractFileName(request.getPart("imagem"));
+			File saveFile = new File(fileName);
+			System.out.println(saveFile.getAbsolutePath());
+			pdfAttachment.attachFile(saveFile.getAbsolutePath());
+			
 			
 			//Attach body parts
 			emailContent.addBodyPart(textBodyPart);
@@ -147,11 +160,62 @@ public class enviaEmpenhoPorEmailESalvaServlet extends HttpServlet {
 			System.out.println("Sent message");
 		} catch (MessagingException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	    
 	    response.sendRedirect("pages/adcSucessoEmpenho.jsp");
     }
+    
+    private List<File> saveUploadedFiles(HttpServletRequest request)
+			throws IllegalStateException, IOException, ServletException {
+		List<File> listFiles = new ArrayList<File>();
+		byte[] buffer = new byte[4096];
+		int bytesRead = -1;
+		Collection<Part> multiparts = request.getParts();
+		if (multiparts.size() > 0) {
+			for (Part part : request.getParts()) {
+				// creates a file to be saved
+				String fileName = extractFileName(part);
+				if (fileName == null || fileName.equals("")) {
+					// not attachment part, continue
+					continue;
+				}
+				
+				File saveFile = new File(fileName);
+				System.out.println("saveFile: " + saveFile.getAbsolutePath());
+				FileOutputStream outputStream = new FileOutputStream(saveFile);
+				
+				// saves uploaded file
+				InputStream inputStream = part.getInputStream();
+				while ((bytesRead = inputStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, bytesRead);
+				}
+				outputStream.close();
+				inputStream.close();
+				
+				listFiles.add(saveFile);
+			}
+		}
+		return listFiles;
+	}
+    private String extractFileName(Part part) {
+		String contentDisp = part.getHeader("content-disposition");
+		String[] items = contentDisp.split(";");
+		for (String s : items) {
+			if (s.trim().startsWith("filename")) {
+				return s.substring(s.indexOf("=") + 2, s.length() - 1);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Deletes all uploaded files, should be called after the e-mail was sent.
+	 */
+	private void deleteUploadFiles(List<File> listFiles) {
+		if (listFiles != null && listFiles.size() > 0) {
+			for (File aFile : listFiles) {
+				aFile.delete();
+			}
+		}
+	}
 }
